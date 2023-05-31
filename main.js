a14f4268dc40a7707e5932e0ac57cf7042d5fd4a
Cade Alexander Heck <cadeheck16@gmail.com>
2023-05-30 22:32:14 -0400

    sphere is now on screen

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
+import './style.css'
 
-//Scence
+//Scene
 const scene = new THREE.Scene();
 
 //Create our sphere
@@ -8,12 +9,24 @@ const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
 })
-
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
 
+//Light
+const light = new THREE.PointLight(0xffffff, .3, 100);
+light.position.set(0, 7, 8)
+scene.add(light)
+
+
+//Sizes
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
 //Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 100);
+camera.position.z = 10
 scene.add(camera)
 
 
@@ -21,6 +34,26 @@ scene.add(camera)
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGL1Renderer({ canvas })
 
-renderer.setSize(800, 600)
+renderer.setSize(size.width, size.height)
 renderer.render(scene, camera)
 
+
+
+//Resize
+window.addEventListener('resize', () => {
+  //Update sizes
+  size.width = window.innerWidth
+  size.height = window.innerHeight
+
+  //update Camera
+  camera.aspect = size.width / size.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(size.width, size.height)
+})
+
+const loop = () => {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+
+loop()
