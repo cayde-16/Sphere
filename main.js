d412e6890aa8bf9308f45886925048b6aeaf7135
Cade Alexander Heck <cadeheck16@gmail.com>
2023-05-31 13:52:40 -0400

    sphere now has rgb when rotating,roughness & more

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import './style.css'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { gsap } from 'gsap';
 
 //Scene
 const scene = new THREE.Scene();
@@ -9,6 +10,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness:0.2
 })
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
@@ -16,6 +18,7 @@ scene.add(mesh)
 //Light
 const light = new THREE.PointLight(0xffffff, .3, 100);
 light.position.set(0, 7, 8)
+light.intensity = 1.25
 scene.add(light)
 
 
@@ -69,5 +72,31 @@ const loop = () => {
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
-
 loop()
+
+//TimeLine Magic
+const timeline = gsap.timeline({ default: {duration: 1} })
+timeline.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
+timeline.fromTo('nav', {y: '-100%'}, {y:'0%'})
+timeline.fromTo('title', {opacity: 0}, {opacity: 1})
+
+//Mouse Animation Color
+let mouseDown = false;
+let rgb = []
+window.addEventListener('mousedown', () => ( mouseDown=true ));
+window.addEventListener('mouseup', () => ( mouseDown=false ));
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+      let rgb = [
+        Math.round((e.pageX / size.width ) * 255),
+        Math.round((e.pageY / size.height ) * 255),
+        150,
+    ]
+    console.log(`r:${rgb[0]},g:${rgb[1]},b:${rgb[2]} `)
+
+    //Let's animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color, {r:newColor.r, g:newColor.g, b:newColor.b})
+  }
+})
