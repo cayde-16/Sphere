a437eca90098e6b9cbbc1e786ffb700e6754b26a
Cade Alexander Heck <cadeheck16@gmail.com>
2023-05-30 21:53:45 -0400

    started working on sphere

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ const material = new THREE.MeshStandardMaterial({
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+scene.add(camera)
+
+
+//Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGL1Renderer({ canvas })
+
+renderer.setSize(800, 600)
+renderer.render(scene, camera)
+
